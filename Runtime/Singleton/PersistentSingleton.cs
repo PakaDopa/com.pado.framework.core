662f68764b763c510549bb3bd802a2ef80888f76
a-a <60053866+PakaDopa@users.noreply.github.com>
2026-03-24 21:59:38 +0900

    Delete Runtime directory

--- Runtime/Singleton/PersistentSingleton.cs
+++
@@ -1,7 +0,0 @@
-namespace Pado.Framework.Core.Singleton
-{
-    public abstract class PersistentSingleton<T> : Singleton<T> where T : UnityEngine.MonoBehaviour
-    {
-        protected override bool ShouldDontDestroyOnLoad => true;
-    }
-}
